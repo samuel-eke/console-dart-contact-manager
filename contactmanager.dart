e0c4657ea1d236e567b37e8e51bb3d2bf1ab9c21
sam <eke.eke.eth@gmail.com>
2026-01-05 16:08:36 +0100

    console based contact manager

---
+++ contactmanager.dart
@@ -0,0 +1,46 @@
+import 'dart:io';
+
+menu() {
+  Map<String, int> contact = {"samuel": 123456};
+  print("---------Perform following functions-------------");
+  print(
+    "1. View contacts \n2. Add new contacts (name and phone number) \n3. search contacts \n4. delete contact \n0. Exit",
+  );
+  print("What do you want to do? ");
+  int? n = int.parse(stdin.readLineSync()!);
+
+  // String? menuChoice = stdin.readLineSync();
+  switch (n) {
+    case 1:
+      if (contact.isEmpty) {
+        print("No contact added yet");
+        print("Add new contact? ('yes/no')");
+        String? addContactOption = stdin.readLineSync();
+        if (addContactOption == "yes") {
+          print("contact added ");
+        } else {
+          break;
+        }
+      } else {
+        print(contact);
+      }
+      break;
+    case 2:
+      print("how many contacts do you want to add?");
+      int? contactToAdd = int.parse(stdin.readLineSync()!);
+      for (int i = 0; i <= contactToAdd; i++) {
+        print("enter name:");
+        String qContactName = stdin.readLineSync()!;
+        print("enter contact phone number");
+        int? qContactPhoneNumber = int.parse(stdin.readLineSync()!);
+        contact[qContactName] = qContactPhoneNumber;
+      }
+      print(contact);
+      break;
+    default:
+  }
+}
+
+void main(List<String> args) {
+  menu();
+}
