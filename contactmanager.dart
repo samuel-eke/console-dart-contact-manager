ab9f85ff83caf739c2b0ff182d5f71f319b3a58c
sam <eke.eke.eth@gmail.com>
2026-01-05 16:31:08 +0100

    all core functions done

--- contactmanager.dart
+++ contactmanager.dart
@@ -28,16 +28,39 @@ menu() {
     case 2:
       print("how many contacts do you want to add?");
       int? contactToAdd = int.parse(stdin.readLineSync()!);
-      for (int i = 0; i <= contactToAdd; i++) {
+      for (int i = 1; i <= contactToAdd; i++) {
         print("enter name:");
         String qContactName = stdin.readLineSync()!;
         print("enter contact phone number");
         int? qContactPhoneNumber = int.parse(stdin.readLineSync()!);
         contact[qContactName] = qContactPhoneNumber;
       }
+      print("$contactToAdd contacts added successfully");
       print(contact);
       break;
+    case 3:
+      print("enter contact you want to search for");
+      String searchContact = stdin.readLineSync()!;
+      if (contact.containsKey(searchContact)) {
+        contact.forEach((k, v) {
+          print("$k - $v");
+        });
+      } else {
+        print("contact not found");
+      }
+      break;
+    case 4:
+      print("which contact will you like to delete?");
+      String delContact = stdin.readLineSync()!;
+      contact.remove(delContact);
+      print("'$delContact' deleted successfully");
+      break;
+
+    case 0:
+      print("goodbye");
+      break;
     default:
+      print("wrong option");
   }
 }
 
